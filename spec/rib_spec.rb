29148a5f1ae2a439bd9a0edb96d249af7d2affe4
boticelli <kandelakitina@gmail.com>
2025-07-03 17:49:43 +0300

    Add self.fibonacci and self.fibs method to Array class methods

---
+++ spec/rib_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# require 'rspec'
+require_relative '../lib/fibs'
+
+RSpec.describe Array do
+  describe '.fibs' do
+    it 'returns an array of Fibonacci numbers' do
+      expect(Array.fibs(8)).to eq([0, 1, 1, 2, 3, 5, 8, 13])
+    end
+
+    it 'returns an empty array for non-positive input' do
+      expect(Array.fibs(0)).to eq([])
+      expect(Array.fibs(-1)).to eq([])
+    end
+
+    it 'returns the first Fibonacci number for input 1' do
+      expect(Array.fibs(1)).to eq([0])
+    end
+
+    it 'returns the first two Fibonacci numbers for input 2' do
+      expect(Array.fibs(2)).to eq([0, 1])
+    end
+  end
+end
